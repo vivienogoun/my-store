5182c083d055720704368d144b7a8802437735ee
Vivien Ogoun <vivienogoun1@gmail.com>
2023-10-14 17:13:22 +0100

    Add footer component and some icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
@@ -17,6 +18,7 @@ import { ConfirmationComponent } from './components/confirmation/confirmation.co
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
     ProductListComponent,
     ProductItemComponent,
     ProductItemDetailComponent,
